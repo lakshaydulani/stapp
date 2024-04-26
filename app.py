a9cdbdb733668884f891173467106c7142aa4370
Lakshay Dulani <Lakshay.Dulani@in.ey.com>
2024-04-26 17:55:14 +0530

    testing

--- app.py
+++ app.py
@@ -31,34 +31,34 @@ if uploaded_file is not None:
         with open(filename, 'wb') as f: 
             f.write(bytes_data)
 
-    # 
-    #     documents_with_instruction = LlamaParse(
-    #                                     result_type="markdown",
-    #                                         parsing_instruction="""
-    #                                     This document is a report.
-    #                                     Output all the tables as it is.""",
-    #                                     ).load_data("samplereport.pdf")
+    
+        documents_with_instruction = LlamaParse(
+                                        result_type="markdown",
+                                            parsing_instruction="""
+                                        This document is a report.
+                                        Output all the tables as it is.""",
+                                        ).load_data("samplereport.pdf")
 
-    #     node_parser_instruction = MarkdownElementNodeParser(llm=OpenAI(model="gpt-3.5-turbo-0125"), num_workers=8)
+        node_parser_instruction = MarkdownElementNodeParser(llm=OpenAI(model="gpt-3.5-turbo-0125"), num_workers=8)
         
-    #     nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction)
-    #     (base_nodes_instruction,objects_instruction,) = node_parser_instruction.get_nodes_and_objects(nodes_instruction)
+        nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction)
+        (base_nodes_instruction,objects_instruction,) = node_parser_instruction.get_nodes_and_objects(nodes_instruction)
 
-    #     recursive_index_instruction = VectorStoreIndex(nodes=base_nodes_instruction + objects_instruction)
+        recursive_index_instruction = VectorStoreIndex(nodes=base_nodes_instruction + objects_instruction)
         
-    #     query_engine_instruction = recursive_index_instruction.as_query_engine(similarity_top_k=25)
+        query_engine_instruction = recursive_index_instruction.as_query_engine(similarity_top_k=25)
     
     
-    # if "messages" not in st.session_state:
-    #     st.session_state.messages = []
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+            
+        if prompt := st.chat_input("Ask your questions about the document"):
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            with st.chat_message("assistant"):
+                response_1_i = query_engine_instruction.query(prompt)
+                st.markdown(response_1_i)
+                st.session_state.messages.append({"role": "assistant", "content": response_1_i})
         
-    # if prompt := st.chat_input("Ask your questions about the document"):
-    #     st.session_state.messages.append({"role": "user", "content": prompt})
-    #     with st.chat_message("user"):
-    #         st.markdown(prompt)
-    #     with st.chat_message("assistant"):
-    #         response_1_i = query_engine_instruction.query(prompt)
-    #         st.markdown(response_1_i)
-    #         st.session_state.messages.append({"role": "assistant", "content": response_1_i})
-    
     
