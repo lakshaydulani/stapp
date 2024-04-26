d27f7aea94c7421ffeb7521af3d4825aa0c2ce98
Lakshay Dulani <lakshaydulani@gmail.com>
2024-04-26 17:09:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,61 +23,56 @@ st.subheader("Upload the file: (PDF only)")
 
 uploaded_file = st.file_uploader("Choose a file")
 
-if uploaded_file is not None:
-    
-    # To read file as bytes:
+if uploaded_file is not None:    
     bytes_data = uploaded_file.getvalue()
-
-    # get current cwd python
     cwd = os.getcwd()
     filename = os.path.join(cwd, "samplereport.pdf")
 
     with open(filename, 'wb') as f: 
         f.write(bytes_data)
 
-    with st.spinner('Wait for it...'):
+    with st.spinner('Working on the document..'):
         documents_with_instruction = LlamaParse(
-    result_type="markdown",
-    parsing_instruction="""
-This document is an inspection report.
-Output all the tables as it is.
-Output the Product photo. """,
-).load_data("samplereport.pdf")
+                                        result_type="markdown",
+                                            parsing_instruction="""
+                                        This document is an inspection report.
+                                        Output all the tables as it is.
+                                        Output the Product photo. """,
+                                        ).load_data("samplereport.pdf")
     
-    
-    
-    st.success('Done!')
-
     st.markdown(documents_with_instruction[0].text)
+    
+    from llama_index.core.node_parser import MarkdownElementNodeParser
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+    node_parser_instruction = MarkdownElementNodeParser(
+    llm=OpenAI(model="gpt-3.5-turbo-0125"), num_workers=8
+)
+    
+    nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction)
+    (
+    base_nodes_instruction,
+    objects_instruction,
+    ) = node_parser_instruction.get_nodes_and_objects(nodes_instruction)
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("Ask your questions about the document"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        st.markdown(prompt)
-
-    # with st.chat_message("assistant"):
-    #     stream = client.chat.completions.create(
-    #         model=st.session_state["openai_model"],
-    #         messages=[
-    #             {"role": m["role"], "content": m["content"]}
-    #             for m in st.session_state.messages
-    #         ],
-    #         stream=True,
-    #     )
-    #     response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": prompt})
+    recursive_index_instruction = VectorStoreIndex(
+        nodes=base_nodes_instruction + objects_instruction
+    )
+    
+    query_engine_instruction = recursive_index_instruction.as_query_engine(
+        similarity_top_k=25
+    )
+    
+    
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        
+    if prompt := st.chat_input("Ask your questions about the document"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        with st.chat_message("assistant"):
+            response_1_i = query_engine_instruction.query(prompt)
+            st.markdown(response_1_i)
+            st.session_state.messages.append({"role": "assistant", "content": response_1_i})
+    
+    
