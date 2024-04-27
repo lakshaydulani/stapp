0500457e9454c8e9011d1606e37537a98a5121b4
Lakshay Dulani <Lakshay.Dulani@in.ey.com>
2024-04-27 21:57:41 +0530

    changes

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import VectorStoreIndex
 from llama_index.core import Settings
 from llama_index.core.node_parser import MarkdownElementNodeParser
+from getimgs import save_images 
+from sentenceunderstanding import isSimilar
+from glob import glob
+
 
 os.environ["LLAMA_CLOUD_API_KEY"] = st.secrets["LLAMA_KEY"]
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
@@ -18,7 +22,6 @@ llm = OpenAI(model="gpt-3.5-turbo-0125")
 Settings.llm = llm
 
 st.title("Inspection Report Analyzer")
-# st.subheader("Upload the file: ")
 
 uploaded_file = st.file_uploader("Choose a file (PDF only)")
 
@@ -27,43 +30,54 @@ if uploaded_file is not None:
     cwd = os.getcwd()
     filename = os.path.join(cwd, "samplereport.pdf")
     
-    with st.spinner('Loading..'):
+    with st.spinner('Working..'):
         with open(filename, 'wb') as f: 
             f.write(bytes_data)
-
-    
-        documents_with_instruction = LlamaParse(
-                                        result_type="markdown",
-                                            parsing_instruction="""
-                                        This document is a report.
-                                        Output all the tables as it is.""",
-                                        ).load_data("samplereport.pdf")
         
-        tab1, tab2 = st.tabs(["Chatbox", "Parsed Report"])
-        tab2.markdown(documents_with_instruction[0].text)
-
-        with tab1:
-            node_parser_instruction = MarkdownElementNodeParser(llm=OpenAI(model="gpt-3.5-turbo-0125"), num_workers=8)
+        save_images()
             
-            nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction)
-            (base_nodes_instruction,objects_instruction,) = node_parser_instruction.get_nodes_and_objects(nodes_instruction)
-
-            recursive_index_instruction = VectorStoreIndex(nodes=base_nodes_instruction + objects_instruction)
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
             
-            query_engine_instruction = recursive_index_instruction.as_query_engine(similarity_top_k=25)
-        
-        
-            if "messages" not in st.session_state:
-                st.session_state.messages = []
+        if prompt := st.chat_input("Ask your questions about the document"):
+            
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            
+         
+
+            documents_with_instruction = LlamaParse(
+                                            result_type="markdown",
+                                                parsing_instruction="""
+                                            This document is a report.
+                                            Output all the tables as it is.""",
+                                            ).load_data("samplereport.pdf")
+            
+            tab1, tab2 = st.tabs(["Chatbox", "Parsed Report"])
+            tab2.markdown()
+            tab2.download_button("Download",documents_with_instruction[0].text)
+
+            with tab1:
+                node_parser_instruction = MarkdownElementNodeParser(llm=OpenAI(model="gpt-3.5-turbo-0125"), num_workers=8)
                 
-            if prompt := st.chat_input("Ask your questions about the document"):
+                nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction)
+                (base_nodes_instruction,objects_instruction,) = node_parser_instruction.get_nodes_and_objects(nodes_instruction)
+
+                recursive_index_instruction = VectorStoreIndex(nodes=base_nodes_instruction + objects_instruction)
                 
-                with st.chat_message("user"):
-                    st.markdown(prompt)
-                st.session_state.messages.append({"role": "user", "content": prompt})
-                response_1_i = query_engine_instruction.query(prompt)
-                with st.chat_message("assistant"):                    
-                    st.markdown(response_1_i)
-                st.session_state.messages.append({"role": "assistant", "content": response_1_i})
+                query_engine_instruction = recursive_index_instruction.as_query_engine(similarity_top_k=25)
             
-    
+            
+                if "messages" not in st.session_state:
+                    st.session_state.messages = []
+                    
+                if(isSimilar(prompt, "show the image")):
+                    with st.chat_message("assistant"):   
+                        st.image("imgs/image1_1.png", caption="image1_1.png")
+                else:
+                    response_1_i = query_engine_instruction.query(prompt)
+                    with st.chat_message("assistant"):                    
+                        st.markdown(response_1_i)
+                    st.session_state.messages.append({"role": "assistant", "content": response_1_i})
+                    
