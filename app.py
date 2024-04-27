c05aaf635b90dd9be4ba3b2516060599915025dc
Lakshay Dulani <Lakshay.Dulani@in.ey.com>
2024-04-27 22:11:35 +0530

    changes

--- app.py
+++ app.py
@@ -36,47 +36,38 @@ if uploaded_file is not None:
         
         save_images()
             
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
-            
-        if prompt := st.chat_input("Ask your questions about the document"):
-            
-            with st.chat_message("user"):
-                st.markdown(prompt)
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            
-         
+    
 
-            documents_with_instruction = LlamaParse(
-                                            result_type="markdown",
-                                                parsing_instruction="""
-                                            This document is a report.
-                                            Output all the tables as it is.""",
-                                            ).load_data("samplereport.pdf")
-            
-            tab1, tab2 = st.tabs(["Chatbox", "Parsed Report"])
-            tab2.download_button("Download",documents_with_instruction[0].text)
+        documents_with_instruction = LlamaParse(
+                                        result_type="markdown",
+                                            parsing_instruction="""
+                                        This document is a report.
+                                        Output all the tables as it is.""",
+                                        ).load_data("samplereport.pdf")
+        
+        tab1, tab2 = st.tabs(["Chatbox", "Parsed Report"])
+        tab2.download_button("Download",documents_with_instruction[0].text)
 
-            with tab1:
-                node_parser_instruction = MarkdownElementNodeParser(llm=OpenAI(model="gpt-3.5-turbo-0125"), num_workers=8)
+        with tab1:
+            node_parser_instruction = MarkdownElementNodeParser(llm=OpenAI(model="gpt-3.5-turbo-0125"), num_workers=8)
+            
+            nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction)
+            (base_nodes_instruction,objects_instruction,) = node_parser_instruction.get_nodes_and_objects(nodes_instruction)
+
+            recursive_index_instruction = VectorStoreIndex(nodes=base_nodes_instruction + objects_instruction)
+            
+            query_engine_instruction = recursive_index_instruction.as_query_engine(similarity_top_k=25)
+        
+        
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
                 
-                nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction)
-                (base_nodes_instruction,objects_instruction,) = node_parser_instruction.get_nodes_and_objects(nodes_instruction)
-
-                recursive_index_instruction = VectorStoreIndex(nodes=base_nodes_instruction + objects_instruction)
+            if(isSimilar(prompt, "show the image")):
+                with st.chat_message("assistant"):   
+                    st.image("imgs/image1_1.png", caption="image1_1.png")
+            else:
+                response_1_i = query_engine_instruction.query(prompt)
+                with st.chat_message("assistant"):                    
+                    st.markdown(response_1_i)
+                st.session_state.messages.append({"role": "assistant", "content": response_1_i})
                 
-                query_engine_instruction = recursive_index_instruction.as_query_engine(similarity_top_k=25)
-            
-            
-                if "messages" not in st.session_state:
-                    st.session_state.messages = []
-                    
-                if(isSimilar(prompt, "show the image")):
-                    with st.chat_message("assistant"):   
-                        st.image("imgs/image1_1.png", caption="image1_1.png")
-                else:
-                    response_1_i = query_engine_instruction.query(prompt)
-                    with st.chat_message("assistant"):                    
-                        st.markdown(response_1_i)
-                    st.session_state.messages.append({"role": "assistant", "content": response_1_i})
-                    
