7bcb26cd33130419d5f5bf6eacea34d0c943b1bc
Lakshay Dulani <Lakshay.Dulani@in.ey.com>
2024-05-10 13:55:08 +0530

    add medical type

--- app.py
+++ app.py
@@ -29,46 +29,23 @@ embed_model = OpenAIEmbedding(model="text-embedding-3-small")
 llm = OpenAI(model="gpt-3.5-turbo-0125")
 Settings.llm = llm
 
-st.title("Report Analyzer")
-
-option = st.selectbox(
-    "What kind of document do you want to analyze?",
-    ("Inspection Report", "Medical Report","General PDF"))
-
-inspection_instruction_with_options = """This document is an inspection report.Preserve all the tables. Only if defects are mentioned in the document, create a separate section named 'Defects' and save information for columns {Critical, Major, Minor} and rows {AQL, Defects found, Max. allowed} in a separate table. Give each section their own heading"""
-
-medical_instruction_with_options = """This document is a Medical blood report of a patient. Ignore Comments given in the document. 
-Whenever there is a table with test result in the document, append it to a table with the following column values {TestName} and their {Result}.
-The output should only have markdown tables prefixed with their heading. The markdown output should be sanitized"""
-
-general_instruction_with_options = """Keep the tables in the document as it is."""
+st.title("Inspection Report Analyzer")
 
 uploaded_file = st.file_uploader("Choose a file (PDF only)")
-
-
 
 if uploaded_file is not None:    
     bytes_data = uploaded_file.getvalue()
     filename = "./samplereport.pdf"
     
-    
     with st.spinner('Working...'):
         with open(filename, 'wb') as f: 
             f.write(bytes_data)
        
-       
-        instruction_with_options = general_instruction_with_options
-        
-        if(option == "Inspection Report"):
-            save_images()
-            instruction_with_options = inspection_instruction_with_options
-        elif(option == "Medical Report"):
-            instruction_with_options = medical_instruction_with_options
-            
-               
+        save_images()       
         documents_with_instruction = LlamaParse(
                                         result_type="markdown",
-                                            parsing_instruction=instruction_with_options,
+                                            parsing_instruction="""
+                                        This document is an inspection report.Preserve all the tables. Only if defects are mentioned in the document, create a separate section named 'Defects' and save information for columns {Critical, Major, Minor} and rows {AQL, Defects found, Max. allowed} in a separate table. Give each section their own heading""",
                                         ).load_data("./samplereport.pdf")
         
         
@@ -76,11 +53,10 @@ if uploaded_file is not None:
             st.error("Some error in processing this report.")
         else:
             ref = documents_with_instruction[0].text
-            # st.markdown(ref)
         
             node_parser_instruction = MarkdownElementNodeParser(llm=OpenAI(model="gpt-3.5-turbo-0125"), num_workers=8)
             
-            nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction[0])
+            nodes_instruction = node_parser_instruction.get_nodes_from_documents(documents_with_instruction)
             (base_nodes_instruction,objects_instruction,) = node_parser_instruction.get_nodes_and_objects(nodes_instruction)
 
             recursive_index_instruction = VectorStoreIndex(nodes=base_nodes_instruction + objects_instruction)
